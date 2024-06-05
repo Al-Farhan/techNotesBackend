13f6f6c2402fde409f3aa46397e14ead9022a1db
Farhan Shaikh <95585017+Al-Farhan@users.noreply.github.com>
2024-06-05 16:47:20 +0530

    Ready for deployment

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) { // We have to remove the !origin in deployment (Basically it is used for services like Postman)
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
